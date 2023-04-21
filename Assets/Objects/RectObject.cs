2edcb379dfd2d2057cd54d76e43782113662119e
Sebastian Misje Jonassen <seb2500@gmail.com>
2023-04-21 20:52:35 +0200

    Refactored hit logic, implemented rotation on all axis

--- Assets/Objects/RectObject.cs
+++ Assets/Objects/RectObject.cs
@@ -15,60 +15,20 @@ namespace Objects
         public Rect GetRect()
         {
             _mesh = meshFilter.sharedMesh;
-
-            var vert = new Vector3[_mesh.vertexCount];
+            
+            Vector3 min = _mesh.bounds.min, max = _mesh.bounds.max;
 
             var t = transform;
+            var lossyScale = t.lossyScale;
 
-            var position = t.position;
-            var rotation = t.rotation.eulerAngles;
+            min.Scale(lossyScale);
+            max.Scale(lossyScale);
+            
+            rect.rotation = Matrix4x4.Transpose(Matrix4x4.Rotate(t.rotation));
+            rect.offset = t.position;
 
-            t.rotation = Quaternion.identity;
-            t.position = Vector3.zero;
-
-            Vector3 min = Vector3.positiveInfinity, max = Vector3.negativeInfinity;
-
-            for (var index = 0; index < _mesh.vertices.Length; index++)
-            {
-                vert[index] = t.TransformPoint(_mesh.vertices[index]);
-                for (var n = 0; n < 3; n++)
-                {
-                    max = Vector3.Max(vert[index], max);
-                    min = Vector3.Min(vert[index], min);
-                }
-            }
-
-            t.rotation = Quaternion.Euler(rotation);
-            t.position = position;
-
-            rotation *= Mathf.PI / 180f;
-
-            rect.sinRotation = new Vector3(Mathf.Sin(rotation.x), Mathf.Sin(rotation.y), Mathf.Sin(rotation.z));
-            rect.cosRotation = new Vector3(Mathf.Cos(rotation.x), Mathf.Cos(rotation.y), Mathf.Cos(rotation.z));
-            rect.offset = position;
-
-            switch (rect.orientation)
-            {
-                case 0:
-                    rect.pos0 = min;
-                    rect.pos1 = max;
-                    rect.k = min.z;
-
-                    rect.scale = t.lossyScale.x;
-                    break;
-                case 1:
-                    rect.pos0 = min;
-                    rect.pos1 = max;
-                    rect.k = min.y;
-                    rect.scale = t.lossyScale.x;
-                    break;
-                case 2:
-                    rect.pos0 = min;
-                    rect.pos1 = max;
-                    rect.k = min.x;
-                    rect.scale = t.lossyScale.x;
-                    break;
-            }
+            rect.pos0 = min;
+            rect.pos1 = max;
 
             return rect;
         }
